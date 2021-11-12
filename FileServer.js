10bebaf5741f67db8edc80f108777ab42aa01f02
AnthonyBower <anthonybower6@yahoo.com>
2021-11-11 19:16:59 -0500

    first commit

---
+++ FileServer.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+    fs = require('fs'),
+    cors = require('cors'),
+    port = 3030,
+    app = express(),
+    dir = 'C:/Users/Anthony/Documents/SWIS_Files/';
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/files/:filename', (req, res) => {
+    //return relevant info from filename
+    console.log("Request received for " + req.params.filename + "!");
+    fs.readFile(dir + req.params.filename, (err, data) => {
+        if(err) {
+            console.log("Error!");
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+
+app.put('/file/:filename', (req, res) => {
+    fs.writeFile(dir + req.params.filename, req.body.body, (err) => {
+        if(err) {
+            res.writeHead(400);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+app.listen(port, () => {
+    console.log("File server running on port " + port);
+});
